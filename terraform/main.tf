b5a0260f06f3e876759340dafac2b1854f2d2783
Max Goldstein <goldstem@oregonstate.edu>
2026-06-02 05:33:28 +0000

    set up security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,35 @@ resource "aws_instance" "minecraft_server" {
 	ami		= data.aws_ami.ubuntu.id
 	instance_type	= "t2.small"
 
+	vpc_security_group_ids = [aws_security_group.minecraft_server-sg.id]
+
 	tags = {
 		Name = "minecraft"
 	}
+}
+
+resource "aws_security_group" "minecraft_server-sg" {
+	name		= "minecraft_server-sg"
+	description	= "sg for minecraft server"
+
+	ingress {
+		from_port	= 22
+		to_port		= 22
+		protocol	= "tcp"
+		cidr_blocks	= ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port	= 25565
+		to_port		= 25565
+		protocol	= "tcp"
+		cidr_blocks	= ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port	= 0
+		to_port		= 0
+		protocol	= "-1"
+		cidr_blocks	= ["0.0.0.0/0"]
+	} 
 } 
